eccfafdfd21c568f6c36c399d18f0820937b9cef
yoSakamoto <sasiko.spiral@gmail.com>
2015-11-29 20:44:25 +0900

    Initial Commit

---
+++ GochiPlaceTests/GochiPlaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  GochiPlaceTests.swift
+//  GochiPlaceTests
+//
+//  Created by 坂本葉 on 2015/11/29.
+//  Copyright © 2015年 YoSakamoto. All rights reserved.
+//
+
+import XCTest
+@testable import GochiPlace
+
+class GochiPlaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
